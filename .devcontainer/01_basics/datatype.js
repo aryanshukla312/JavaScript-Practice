995202cb62721ed266fe90e33921250410c9dbd9
Aryan shukla <78729393+aryanshukla312@users.noreply.github.com>
2024-03-07 14:00:57 +0000

    container config done

---
+++ .devcontainer/01_basics/datatype.js
@@ -0,0 +1,16 @@
+// code readibility should be high you should write clean code
+
+// console.log(3+
+//     3)this will give 6 but code readibility is very bad
+
+// data types in JavaScript number ->2 power 53 String-> double quote
+// boolean-> true or false null-> StandAlone value and Undefined-> when you does not intialize variable
+
+// symbol-> to uniqueness
+
+
+
+
+console.log(typeof "Aryan")
+console.log(typeof null)
+// type of null here is object this is in JavaScript
