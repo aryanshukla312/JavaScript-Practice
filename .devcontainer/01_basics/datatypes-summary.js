27ea488b768868eb0b1616aeff38233fb078cecb
Aryan shukla <78729393+aryanshukla312@users.noreply.github.com>
2024-03-12 20:06:17 +0000

    container config done

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,54 @@
+// Non Primitive: String , Number, Null, Boolean , Undefined ,symbol , BigInt
+
+// Reference Type: Arrays, Object Functions
+
+// Arrays
+// const Heroes=["IronMan","Captain America" , "Scarlett Witch"]
+
+// // objects
+// let myobj={
+//     name:"Aryan",
+//     age:22,
+// }
+// // function\
+// const fun=function(){
+//     console.log("Hi I am here")
+
+// }
+// const name="Aryan"
+// const a=50
+// console.log(name + a +"value")
+
+// console.log(`Hello my name is ${name}`)
+
+// const str1=new String("Aryan   ")
+// console.log(str1.concat(" shukla"))
+// console.log(str1.charAt(4))
+// const URL="Aryan.com%20"
+// URL.replace('%20','.')
+// console.log(URL.replace('%20','.'))
+// console.log(str1.split())
+
+// Numbers and Maths:-------------
+// const num=10
+// console.log(num)
+// const num2=new Number(100)
+// console.log(num2.toString)
+// console.log(typeof num2)
+// const pre=1123.8896
+// console.log(pre.toPrecision(3))
+
+// Maths Library:::--------
+// console.log(Math.abs(-4))
+
+// console.log(Math.floor(4.32))
+// value of randomfunction lie between 0 to 1
+
+// if you dont want your value 0 Math.random()+1
+
+console.log(Math.random()*10)
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1)))
+
---
+++ .devcontainer/01_basics/typeconversion.js
@@ -0,0 +1,23 @@
+// let a ="33abg"
+// let b =null
+
+// a=Number(a)
+
+// console.log(typeof (a) + " " + typeof b);
+// console.log(b)
+
+// let value=" Aryan "
+// let num=5;
+// let res=value+num
+// console.log(1+1+res)
+// console.log("2">1)
+
+// let value=3
+// let negvalue=-value;
+// console.log(negvalue)
+// console.log("1"+2)
+// console.log(1+1+6+"5")
+
+console.log("02">1)
+console.log("2"==2)
+console.log("2"===2)
